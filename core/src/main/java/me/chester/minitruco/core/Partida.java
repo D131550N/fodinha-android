756f1d6c0c6d7fa50611fe41bc5ed7375a895ad6
D131550N <dieissoncesar@gmail.com>
2026-04-04 20:50:27 -0300

    Update Partida.java

--- core/src/main/java/me/chester/minitruco/core/Partida.java
+++ core/src/main/java/me/chester/minitruco/core/Partida.java
@@ -1,125 +1,71 @@
 package me.chester.minitruco.core;
 
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright © 2005-2023 Carlos Duarte do Nascimento "Chester" <cd@pobox.com> */
+/* Modificado para o jogo Fodinha */
 
 /**
- * Representa uma partida de truco.
+ * Representa o esqueleto de uma partida de Fodinha.
  * <p>
- * As implementações desta classe irão cuidar de executar o jogo (no caso de
- * <code>PartidaLocal</code>) ou manter a comunicação com uma partida em execução
- * remota (<code>PartidaRemota</code>). Em qualquer caso, os objetos Jogador não
- * terão ciência de onde a partida está se passando.
- * <p>
- * Recebe <i>comandos</i> dos jogadores através de seus métodos (ex.:
- * <code>jogaCarta()</code>, <code>aumentaAposta()</code>) e envia <i>notificações</i>
- * a eles chamando métodos deles (Ex.: <code>inicioPartida()</code>, <code>cartaJogada()</code>).
- * <p>
- * A classe é um <code>Runnable</code> para permitir tanto a execução em Thread
- * (que "viverá" o tempo de uma partida completa) quanto a chamada direta ao
- * método <code>run()</code> (para rodar um teste, por exemplo).
- *
- * @see Jogador
+ * Recebe comandos dos jogadores (ex: fazer palpite, jogar carta) e
+ * envia notificações (ex: início de mão, carta jogada, atualização de vidas).
  */
 public abstract class Partida implements Runnable {
 
-    /**
-     * Referência para determinar a ordem das cartas no truco
-     */
-    protected static final String letrasOrdenadas = "4567QJKA23";
-
-    /**
-     * Modalidade do jogo (paulista, mineiro, baralho limpo, manilha velha...)
-     */
     protected final Modo modo;
 
     /**
-     * Rodada que estamos jogando (de 1 a 3).
-     * <p>
-     * (as implementações devem manter atualizado)
+     * Quantidade de cartas distribuídas na rodada atual (1, 2, 3...)
      */
-    int numRodadaAtual;
-
-    public Partida(Modo modo) {
-        this.modo = modo;
-    }
+    protected int quantidadeCartasRodada = 1;
 
     /**
-     * Calcula um valor relativo para a carta, considerando as manilhas em jogo
-     * <p>
-     * Este método está na superclasse porque, no início da rodada, toda a
-     * informação necessária consiste na manilha e em sua regra, e essas já
-     * foram transmitidas, evitando assim, dúzias de comandos.
-     *
-     * @param c
-     *            Carta cujo valor desejamos
-     */
-    public static int getValorTruco(Carta c, char letraManilha) {
-
-        if (c.isFechada()) {
-            // Cartas fechadas sempre têm valor 0
-            return 0;
-        }
-
-        if (letraManilha == SituacaoJogo.MANILHA_VELHA) {
-            if (c.getLetra() == '7' && c.getNaipe() == Carta.NAIPE_OUROS) {
-                return 11;
-            } else if (c.getLetra() == 'A'
-                    && c.getNaipe() == Carta.NAIPE_ESPADAS) {
-                return 12;
-            } else if (c.getLetra() == '7' && c.getNaipe() == Carta.NAIPE_COPAS) {
-                return 13;
-            } else if (c.getLetra() == '4' && c.getNaipe() == Carta.NAIPE_PAUS) {
-                return 14;
-            }
-        }
-
-        if (c.getLetra() == letraManilha) {
-            // Valor de 11 a 14, conforme o naipe
-            switch (c.getNaipe()) {
-            case Carta.NAIPE_OUROS:
-                return 11;
-            case Carta.NAIPE_ESPADAS:
-                return 12;
-            case Carta.NAIPE_COPAS:
-                return 13;
-            case Carta.NAIPE_PAUS:
-                return 14;
-            default:
-                return 0;
-            }
-        } else {
-            // Valor de 1 a 10 conforme a letra
-            return letrasOrdenadas.indexOf(c.getLetra()) + 1;
-        }
-    }
-
-    /**
-     * Jogadores adicionados a esta partida
-     */
-    protected final Jogador[] jogadores = new Jogador[4];
-
-    /**
-     * Número de jogadores adicionados até agora
+     * Total de jogadores na mesa (pode variar de 2 a 6)
      */
     protected int numJogadores = 0;
 
     /**
-     * Guarda quais cartas foram jogadas em cada rodada.
-     * <p>
-     * (as implementações devem alimentar este array)
+     * Jogadores adicionados a esta partida (índices de 1 a 6 são os válidos, o 0 é ignorado)
      */
-    protected Carta[][] cartasJogadasPorRodada;
+    protected final Jogador[] jogadores = new Jogador[7];
 
-    public static String textoModo(String modo) {
-        switch (modo) {
-            case "P": return "Truco Paulista";
-            case "M": return "Truco Mineiro";
-            case "L": return "Baralho Limpo";
-            case "V": return "Manilha Velha";
-            case "1": return "DEBUG: Finaliza em 1";
+    /**
+     * Guarda as vidas de cada jogador. Começam com 2 vidas.
+     */
+    protected int[] vidas = new int[7];
+
+    /**
+     * Guarda o palpite (quantas promessas de vitória) cada jogador fez na mão atual.
+     */
+    protected int[] palpites = new int[7];
+
+    /**
+     * Guarda quantas rodadas cada jogador efetivamente ganhou na mão atual.
+     */
+    protected int[] feitas = new int[7];
+
+    /**
+     * Array para verificar se um jogador já foi eliminado (zerou vidas).
+     */
+    protected boolean[] eliminado = new boolean[7];
+
+    /**
+     * Carta temporária apenas para manter compatibilidade de eventos na interface gráfica,
+     * caso o frontend original exija saber se há algo na mesa. Não afeta a regra da Fodinha.
+     */
+    public Carta cartaDaMesa;
+
+    /**
+     * Indica se a partida foi finalizada.
+     */
+    public boolean finalizada = false;
+
+    public Partida(Modo modo) {
+        this.modo = modo;
+        // Inicializa todos com 2 vidas, do jogador 1 ao 6
+        for (int i = 1; i <= 6; i++) {
+            vidas[i] = 2;
+            eliminado[i] = false;
         }
-        return null;
     }
 
     public Modo getModo() {
@@ -127,202 +73,63 @@ public abstract class Partida implements Runnable {
     }
 
     /**
-     * Inicia o jogo.
-     * <p>
-     * Este método só vai retornar quando a partida for encerrada, então
-     * não deve ser chamado em threads que não querem esperar (Ex.: UI Thread)
+     * Inicia o jogo. O laço principal rodará na subclasse (ex: PartidaLocal)
      */
     public abstract void run();
 
     /**
-     * Informa à partida que o jogador quer descartar aquela carta.
-     * <p>
-     * Tem que ser a vez dele e não pode haver ninguém trucando.
-     * <p>
-     * A rotina não verifica se o jogador realmente possuía aquela carta -
-     * assume-se que as instâncias de Jogador são honestas e se protegem de
-     * clientes remotos desonestos
+     * Informa à partida qual foi o palpite do jogador (quantas ele promete fazer).
      *
+     * @param j Jogador que está palpitando
+     * @param palpite Quantidade prometida
+     */
+    public abstract void fazPalpite(Jogador j, int palpite);
+
+    /**
+     * Informa à partida que o jogador quer descartar aquela carta.
      */
     public abstract void jogaCarta(Jogador j, Carta c);
 
     /**
-     * Informa à partida a resposta daquele jogador a uma mão de 10/11
-     *
-     * @param j
-     *            Jogador que está respondendo
-     * @param aceita
-     *            true se o jogador topa jogar, false se deixar para o parceiro
-     *            decidir
-     */
-    public abstract void decideMaoDeX(Jogador j, boolean aceita);
-
-    /**
-     * Informa à partida que o jogador solicitou um aumento de aposta ("truco",
-     * "seis", etc.).
-     * <p>
-     * Os jogadores são notificados, e a aposta será efetivamente aumentada se
-     * um dos adversários responder positivamente.
-     * <p>
-     * Observe-se que a vez do jogador fica "suspensa", já que lançamentos de
-     * cartas só são aceitos se não houver ninguém trucando. Como o jogador
-     * atualmente só pode trucar na sua vez, isso não é problema.
-     *
-     * @param j
-     *            Jogador que está solicitando o aumento
-     */
-    public abstract void aumentaAposta(Jogador j);
-
-    /**
-     * Informa à partida que o jogador respondeu a um pedido de aumento de aposta
-     *
-     * @param j
-     *            Jogador que respondeu ao pedido
-     * @param aceitou
-     *            <code>true</code> se ele mandou descer, <code>false</code> se
-     *            correu
-     */
-    public abstract void respondeAumento(Jogador j, boolean aceitou);
-
-    /**
-     * Retorna as cartas jogadas por cada jogador naquela rodada
-     *
-     * @param rodada
-     *            número de 1 a 3
-     * @return cartas jogadas naquela rodada (índice = posição do Jogador-1)
-     */
-    public Carta[] getCartasDaRodada(int rodada) {
-        return cartasJogadasPorRodada[rodada - 1];
-    }
-
-    /**
-     * Carta que determina a manilha (em partida que não usa manilha velha)
-     */
-    public Carta cartaDaMesa;
-
-    /**
-     * Atualiza um objeto que contém a situação da partida (exceto pelas cartas do
-     * jogador)
-     *
-     * @param s
-     *            objeto a atualizar
-     * @param j
-     *            Jogador que receberá a situação
+     * Atualiza um objeto que contém a situação da partida.
      */
     public abstract void atualizaSituacao(SituacaoJogo s, Jogador j);
 
-    protected int getValorTruco(Carta c) {
-        return getValorTruco(c, this.getManilha());
-    }
-
     /**
      * Adiciona um jogador a esta partida.
-     * <p>
-     * Ele será colocado na próxima posição disponível, e passa a receber
-     * eventos da partida.
-     *
-     * @param jogador
-     *            Jogador que será adicionado (bot, humano, etc)
-     * @return true se adicionou o jogador, false se não conseguiu (ex.: mesa
-     *         lotada)
+     * Na Fodinha, permite até 6 jogadores.
      */
     public synchronized boolean adiciona(Jogador jogador) {
-
-        // Se for jogador, só entra se a mesa ainda tiver vaga.
-        if (numJogadores == 4) {
+        // Se a mesa lotou, não entra mais ninguém
+        if (numJogadores == 6) {
             return false;
         }
 
-        // Adiciona na lista e notifica a todos (incluindo ele) de sua presença
-        jogadores[numJogadores] = jogador;
         numJogadores++;
+        jogadores[numJogadores] = jogador;
         jogador.partida = this;
         jogador.setPosicao(numJogadores);
+
         for (Jogador j : jogadores) {
             if (j != null) {
                 j.entrouNoJogo(jogador, this);
             }
         }
         return true;
-
     }
 
     /**
      * Recupera um jogador inscrito
      *
-     * @param posicao
-     *            valor de 1 a 4
+     * @param posicao valor de 1 a 6
      * @return Objeto correspondente àquela posição
      */
     public Jogador getJogador(int posicao) {
-        return jogadores[posicao - 1];
-    }
-
-    private char manilha;
-
-    /**
-     * Pontos de cada equipe na partida.
-     */
-    protected final int[] pontosEquipe = { 0, 0 };
-
-    /**
-     * Indica que a partida foi finalizada (para evitar que os bots fiquem
-     * "rodando em falso" caso a partida seja abortada
-     */
-    public boolean finalizada = false;
-
-    /**
-     * @return Letra correspondente à manilha, ou constante em caso de manilha
-     *         fixa
-     * @see SituacaoJogo#MANILHA_VELHA
-     */
-    public char getManilha() {
-        return manilha;
-    }
-
-    /**
-     * Determina a letra da manilha, baseado na carta virada (o "vira").
-     * <p>
-     * Deve ser chamado a cada inicialização de mão.
-     *
-     * @param c
-     *            Carta virada. Ignorado se for partida com manilha velha
-     */
-    public void setManilha(Carta c) {
-
-        cartaDaMesa = c;
-
-        if (modo.isManilhaVelha()) {
-            manilha = SituacaoJogo.MANILHA_VELHA;
-            return;
-        }
-
-        int posManilha = letrasOrdenadas.indexOf(c.getLetra()) + 1;
-        if (posManilha == letrasOrdenadas.length()) {
-            posManilha = 0;
-        }
-        manilha = letrasOrdenadas.charAt(posManilha);
-
-        // Detalhe: no baralho limpo, a manilha do vira 3 é a dama (e não o 4)
-        if (modo.isBaralhoLimpo() && c.getLetra() == '3') {
-            manilha = 'Q';
-        }
-
-    }
-
-    /**
-     * Informa se não estamos impedidos de disponibilizar aumento (por conta
-     * de ser uma mão de 10/11, ou mesmo um empate acima do limite, ex.:
-     * 11x11 no Truco Paulista)
-     */
-    public boolean isPlacarPermiteAumento() {
-        int max = modo.pontuacaoParaMaoDeX();
-        return pontosEquipe[0] < max && pontosEquipe[1] < max;
+        return jogadores[posicao];
     }
 
     /**
      * @return true se esta partida não envolve nenhuma comunicação remota
-     * (bluetooth, internet, etc.)
      */
     public boolean semJogadoresRemotos() {
         return false;
@@ -330,49 +137,23 @@ public abstract class Partida implements Runnable {
 
     /**
      * Aborta a partida por iniciativa daquele jogador
-     *
-     * @param posicao
-     *            posição (1 a 4) do jogador que abandonou o jogo
      */
-     public abstract void abandona(int posicao);
+    public abstract void abandona(int posicao);
 
-    /**
-     * Configuração que faz o jogador humano jogar automaticamente
-     *
-     * @return false (a não ser em PartidaLocal _se_ a opção for habilitada)
-     */
     public boolean isJogoAutomatico() {
         return false;
     }
 
     /**
-     * Retorna o nome usado para um determinado valor quando estamos pedindo aumento
-     * de aposta. Por exemplo, 3 (no truco paulista) ou 4 (no truco mineiro)
-     * se chamam "truco".
-     * <p>
-     * O principal uso é mapear valores para assets do strings.xml. Por exemplo, as
-     * frases usadas no pedido de truco estão em "balao_aumento_truco", para pedir
-     * seis estão em "balao_aumento_seis", etc.
-     *
-     * @param valor aumento solicitado. Pode ser 3, 6, 9, 12 no truco paulista,
-     *              ou 4, 6, 10, 12 no truco mineiro.
-     * @return "truco", "seis", "nove", etc.
+     * Função auxiliar para saber quantos jogadores ainda têm vidas.
      */
-    public String nomeNoTruco(int valor) {
-        switch (valor) {
-            case 3:
-            case 4:
-                return "truco";
-            case 6:
-                return "seis";
-            case 9:
-                return "nove";
-            case 10:
-                return "dez";
-            case 12:
-                return "doze";
+    public int getJogadoresVivos() {
+        int vivos = 0;
+        for (int i = 1; i <= numJogadores; i++) {
+            if (!eliminado[i]) {
+                vivos++;
+            }
         }
-        throw new IllegalArgumentException(valor + " não é um valor especial no truco");
+        return vivos;
     }
-
 }
