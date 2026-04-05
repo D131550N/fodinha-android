533cb7d914aad1f6d9532c673834d5fd381c32e0
D131550N <dieissoncesar@gmail.com>
2026-04-04 21:01:59 -0300

    Update JogadorHumano.java

--- core/src/main/java/me/chester/minitruco/core/JogadorHumano.java
+++ core/src/main/java/me/chester/minitruco/core/JogadorHumano.java
@@ -1,11 +1,17 @@
 package me.chester.minitruco.core;
 
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright © 2005-2023 Carlos Duarte do Nascimento "Chester" <cd@pobox.com> */
+/* Modificado para o jogo Fodinha */
 
 /**
- * Esta superclasse só existe para que o core possa identificar instâncias do jogador humano
- * (que é efetivamente implementado no Android ou em qualquer outra plataforma futura)
+ * Esta superclasse só existe para que o core possa identificar instâncias do jogador humano.
+ * <p>
+ * A implementação real (exibição das cartas, botões de palpite, animações, etc.)
+ * ocorre na camada de Interface Gráfica (Android).
  */
 public abstract class JogadorHumano extends Jogador {
+    
+    // A implementação dos métodos herdados de Jogador.java (vez, inicioMao, etc.)
+    // será feita na classe que herdar desta lá na pasta do Android.
+    
 }
